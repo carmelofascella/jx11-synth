a5d1f82c9fe8b3d1232dfc2152378954c5fcc922
carmelofascella <fascellacarmelo1@gmail.com>
2023-12-22 14:55:04 +0100

    implement BLIT algorithm for saw oscillator

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -10,60 +10,73 @@
 
 #pragma once
 
+const float PI_OVER_4 = 0.7853981633974483f;
+const float PI = 3.1415926535897932f;
 const float TWO_PI = 6.2831853071795864f;
 
 class Oscillator
 {
 public:
-    float amplitude;
-    float inc;
-    float phase;
+    float period = 0.0f;
+    float amplitude = 1.0f;
     
-    float freq;
-    float sampleRate;
-    float phaseBL;
     
     void reset()
     {
+        inc = 0.0f;
         phase = 0.0f;
-        phaseBL = -0.5f;
+        dc = 0.0f;
     }
-    
-    float nextBandlimitedSample()
-    {
-        phaseBL += inc;
-        if (phaseBL >= 1.0f) {
-            phaseBL -= 1.0f;
-        }
-        
-        float output = 0.0f;
-        float nyquist = sampleRate / 2.0f;
-        float h = freq;
-        float i = 1.0f;
-        float m = 0.6366197724f;
-        while (h < nyquist) {
-            output += m * std::sin(TWO_PI * phaseBL * i) / i;
-            h += freq;
-            i += 1.0f;
-            m = -m;
-        }
-        
-        return output;
-    }
+
     
     float nextSample()
     {
-        /*
+        float output = 0.0f;
+
         phase += inc;
-        if ( phase >= 1.0f ) {
-            phase -= 1.0f;
+        if (phase <= PI_OVER_4) {
+            float halfPeriod = period / 2.0f;
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+
+            dc = 0.5f * amplitude / phaseMax;
+            phaseMax *= PI;
+
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+
+            sin0 = amplitude * std::sin(phase);
+            sin1 = amplitude * std::sin(phase - inc);
+            dsin = 2.0f * std::cos(inc);
+
+            if (phase*phase > 1e-9) {
+                output = sin0 / phase;
+            } else {
+                output = amplitude;
+            }
+        } else {
+            if (phase > phaseMax) {
+                phase = phaseMax + phaseMax - phase;
+                inc = -inc;
+            }
+
+            float sinp = dsin * sin0 - sin1;
+            sin1 = sin0;
+            sin0 = sinp;
+
+            output = sinp / phase;
         }
-        
-        float aliased = 2.0f * phase - 1.0f;
-        
-        return amplitude * (aliased - nextBandlimitedSample());
-         */
-        
-        return amplitude * nextBandlimitedSample();
+
+        return output - dc;
     }
+    
+private:
+    float phase;
+    float phaseMax;
+    
+    float sin0;
+    float sin1;
+    float dsin;
+    float inc;
+    
+    float dc;
 };
