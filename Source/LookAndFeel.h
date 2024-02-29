5b352f0a01a04fe1c61aa15d38e625024158e6ae
carmelofascella <fascellacarmelo1@gmail.com>
2024-02-29 14:48:22 +0100

    add cmake configuration files

--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -15,8 +15,8 @@
 class LookAndFeel : public juce::LookAndFeel_V4 {
 public:
     LookAndFeel();
-    
-    
+
+
     void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
                           float sliderPos, float rotaryStartAngle,
                           float rotaryEndAngle, juce::Slider& slider) override;
